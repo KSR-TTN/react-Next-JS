ef24172719855648b07ce488f6fb20084b3515f5
KSR-TTN <kapil.rathore@tothenew.com>
2025-02-17 14:31:52 +0530

    react day 1 assignmen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import MyClassComponent from './components/classComponent';
+import MyFunctionComponent from './components/functionComponent';
+import IdentifyIssueAndFix from './components/identifyIssueAndFix';
+import DisplayHelloWorld from './components/displayHelloWorld';
+import SimpleComponent from './components/simpleComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+      {/* How to set up a React project and render a simple component. */}
+      <SimpleComponent></SimpleComponent>
+
+      {/* Create a simple React app that displays "Hello, World!" on the screen.*/}
+      <DisplayHelloWorld></DisplayHelloWorld>
+
+      {/* Write a functional component that displays the text "Functional Component" and a class component that displays the text "Class Component". Then render both components in the App component. */}
+      <MyClassComponent></MyClassComponent>
+      <MyFunctionComponent></MyFunctionComponent>
+
+      {/* Given the following code, identify the issue and fix it: const element = <h1>Hello, World!;</h1>; */}
+      <IdentifyIssueAndFix></IdentifyIssueAndFix>
       </header>
+     
     </div>
+
   );
 }
 
